2eac1aea1bbe0eed58d31bc0c8c6c840ab8f05a7
Linda-Bley <lindaleyrosario@gmail.com>
2022-09-22 14:01:09 -0400

    cambios

--- src/services/api.js
+++ src/services/api.js
@@ -15,11 +15,11 @@ function CONFIG(keyStores, networkId) {
   case 'mainnet':
     return {
       networkId: 'mainnet',
-      nodeUrl: 'https://rpc.mainnet.near.org',
       keyStore: keyStores,
-      walletUrl: 'https://wallet.near.org',
-      helperUrl: 'https://helper.mainnet.near.org',
-      explorerUrl: 'https://explorer.mainnet.near.org',
+      nodeUrl: "https://rpc.mainnet.near.org",
+      walletUrl: "https://wallet.mainnet.near.org",
+      helperUrl: "https://helper.mainnet.near.org",
+      explorerUrl: "https://explorer.mainnet.near.org",
     }
   case 'testnet':
     return {
@@ -28,7 +28,7 @@ function CONFIG(keyStores, networkId) {
       nodeUrl: "https://rpc.testnet.near.org",
       walletUrl: "https://wallet.testnet.near.org",
       helperUrl: "https://helper.testnet.near.org",
-      explorerUrl: "https://explorer.testnet.near.org"
+      explorerUrl: "https://explorer.testnet.near.org",
     }
   default:
     throw Error(`Unconfigured environment '${NETWORK}'`)
